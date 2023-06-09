b0120239387840a5b0f159cc354a241e5da47776
augustohog <agusto.henriquee@gmail.com>
2023-06-09 19:23:40 -0300

    Protegendo aplicação contra serialização

--- src/main/java/com/augusto/securegate/domain/Porteiro.java
+++ src/main/java/com/augusto/securegate/domain/Porteiro.java
@@ -7,11 +7,13 @@ import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 
 import com.augusto.securegate.domain.enums.Perfil;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Porteiro extends Pessoa {
 	private static final long serialVersionUID = 1L;
 	
+	@JsonIgnore
 	@OneToMany(mappedBy = "porteiro")
 	private List<Controle> controles = new ArrayList<>();
 

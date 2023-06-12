d2dba81bbc487c66783c7d0f649a786a60a108a0
augustohog <agusto.henriquee@gmail.com>
2023-06-12 18:25:31 -0300

    Validação nos campos de criação de novo usuario

--- src/main/java/com/augusto/securegate/resources/PorteiroResource.java
+++ src/main/java/com/augusto/securegate/resources/PorteiroResource.java
@@ -4,6 +4,8 @@ import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -43,7 +45,7 @@ public class PorteiroResource {
 	}
 	
 	@PostMapping
-	public ResponseEntity<PorteiroDTO> create(@RequestBody PorteiroDTO objDTO){
+	public ResponseEntity<PorteiroDTO> create(@Valid @RequestBody PorteiroDTO objDTO){
 		Porteiro newObj = service.create(objDTO);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newObj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
